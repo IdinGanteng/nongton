11791e8165edf1c13f0e37065935922871371add
IdinGanteng <kanggayus101@gmail.com>
2023-07-20 16:44:44 +0700

    shdgf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Game from "./routes/Game";
-import Home from "./routes/Home";
-import FilmList from "./routes/FilmList";
+
 import React from 'react'
 
 const App = () => {
   return (
-    <FilmList/>
+    <h1>asjdhsjh</h1>
   )
 }
 
