a6653957bd58b5a594c6feac7af21f9d9c19f823
IdinGanteng <kanggayus101@gmail.com>
2023-07-23 08:24:12 +0700

    jsdgfj

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import Login from "./Login";
-import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
-import Verify from "./Verify";
 
-const App = () => {
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+function App() {
   return (
-    <div className="bg">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/verifikasi" element={<Verify/>}/>
-        </Routes>
-      </Router>
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
